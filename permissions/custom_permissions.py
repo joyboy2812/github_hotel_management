2fd5d11cf3c51c8e3f2f972577451f6ce61d454e
joyboy2812 <tth281221@gmail.com>
2023-08-02 19:36:21 +0700

    CRUD user

--- permissions/custom_permissions.py
+++ permissions/custom_permissions.py
@@ -9,3 +9,27 @@ class IsAdminUser(BasePermission):
 
         # Kiểm tra xem người dùng có vai trò là "admin" hay không
         return request.user.profile.role.role_name == 'admin'
+
+
+class IsManagerUser(BasePermission):
+    def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
+
+        return request.user.profile.role.role_name == 'manager'
+
+
+class IsStaffUser(BasePermission):
+    def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
+
+        return request.user.profile.role.role_name == 'staff'
+
+
+class IsRegisteredUser(BasePermission):
+    def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
+
+        return request.user.profile.role.role_name == 'registered user'
